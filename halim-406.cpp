62efa7d25e6f1f1607740139e3bdd8fc41e78fe9
duanr1 <Leuchteintraum@gmail.com>
2018-11-27 00:35:05 -0800

    Why 1 is a prime number :)

--- halim-406.cpp
+++ halim-406.cpp
@@ -1,49 +1,59 @@
+#include<cstdio>
 
-#include <iostream>
-#include <cstdlib>
-#include <cstring>
-#include <cstdio>
- 
-using namespace std;
- 
-int used[1001];
-int prim[1001];
- 
-int main()
-{
-	memset( used, 0, sizeof(used) );
-	int count = 0;
-	prim[count ++] = 1;
-	for ( int i = 2 ; i <= 1000 ; ++ i )
-		if ( !used[i] ) {
-			prim[count ++] = i;
-			for ( int j = i<<1 ; j <= 1000 ; j += i )
-				used[j] = 1;
-		}
-	prim[count] = 1001;
-	
-    int n,m;
-    while ( ~scanf("%d%d",&n,&m) ) {
-		printf("%d %d:",n,m);
-		int r = 0;
-		while ( prim[r+1] <= n ) r ++;
-		int mid = r/2;
-		int l = mid;
-		r = mid+(r%2);
-		while ( -- m ) {
-			l --; r ++;
-			if ( l < 0 ) l ++;
-			if ( prim[r] > n ) r --;
-		}
-		while ( l <= r )
-			printf(" %d",prim[l ++]);
-		printf("\n\n");
-    }
-    return 0;
+int prime[169] = { 0 };
+
+bool is_prime(int n) {
+	for (int i = 3; i * i <= n; i = i + 2) {
+		if (n % i == 0) return false;
+	}
+	return true;
 }
 
---------------------- 
-作者：小白菜又菜 
-来源：CSDN 
-原文：https://blog.csdn.net/mobius_strip/article/details/33522257 
-版权声明：本文为博主原创文章，转载请附上博文链接！
+void find_prime() {
+	prime[0] = 1;
+	prime[1] = 2;
+	int cur = 2;
+	for (int i = 3; i < 1000; i = i + 2) {
+		if (is_prime(i)) {
+			prime[cur] = i;
+			cur++;
+		}
+	}
+}
+
+int binary_search(int val, int l, int r) {
+	if (r >= l) {
+		int mid = (l + r) / 2;
+		if (prime[mid] == val) {
+			return mid;
+		}
+		if (prime[mid] > val) {
+			return binary_search(val, l, mid - 1);
+		}
+		return binary_search(val, mid + 1, r);
+	}
+	if (prime[r] > val) return r - 1;
+	else return r;
+}
+
+int main() {
+	find_prime();
+	int n, c;
+	while (scanf("%d%d", &n, &c) == 2) {
+		int index = binary_search(n, 0, 168);
+		int start = index / 2, end;
+		if (index % 2 == 0) {
+			end = start;
+		}
+		else end = start + 1;
+		start -= c - 1;
+		end += c - 1;
+		if (start < 0) start = 0;
+		if (end > index) end = index;
+		printf("%d %d:", n, c);
+		for (int i = start; i <= end; ++i) {
+			printf(" %d", prime[i]);
+		}
+		printf("\n\n");
+	}
+}
